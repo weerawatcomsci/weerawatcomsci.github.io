9c63bcc86515f34daa9e7ba64dd2cb05bd25e0f9
Tom Weerawat <tomweerawat.cs@gmail.com>
2017-04-20 21:04:17 +0700

    Add files via upload

---
+++ eveandboy/payment.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>payment</title>
+<style type="text/css">
+body {
+	background-image: url(image/BG.jpg);
+	text-align: center;
+}
+.ceee {
+	text-align: center;
+	color: #FFF;
+}
+
+a:link {
+	color: #FFF;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #FFF;
+}
+a:hover {
+	text-decoration: none;
+	color: #CF0;
+}
+a:active {
+	text-decoration: none;
+}
+.aa {
+	color: #FFF;
+}
+</style>
+<script type="text/javascript">
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+</script>
+</head>
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center"><img src="image/banner.jpg" width="1000" height="179" /></td>
+  </tr>
+  <tr>
+    <td class="ceee">| &nbsp;<span class="as"><a href="home.html">Home&nbsp;</a> |&nbsp; <a href="aboutus.html">About Us</a> &nbsp;|&nbsp;<a href="catalog-skincares.html">Catalog Products</a> &nbsp;| &nbsp;<a href="payment.php">Payment</a> &nbsp;|&nbsp;<a href="contomctus.html">Contact Us&nbsp;</a> |</span></td>
+  </tr>
+  <tr>
+    <td><table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td colspan="3" align="center"><br />
+          <form id="form1" name="form1" method="post" action="payment1.php">
+            <table width="500" border="0" align="center" cellpadding="0" cellspacing="5">
+              <tr>
+                <td colspan="2" align="right" class="aa">Sent to ::</td>
+                <td width="300"><input type="text" name="textfield" id="textfield" /></td>
+              </tr>
+              <tr>
+                <td colspan="2" align="right" class="aa">From ::</td>
+                <td width="300"><input type="text" name="textfield2" id="textfield2" /></td>
+              </tr>
+              <tr>
+                <td colspan="2" align="right" class="aa">E-Mail ::</td>
+                <td width="300"><input type="text" name="textfield3" id="textfield3" /></td>
+              </tr>
+              <tr>
+                <td height="92" colspan="2" align="right" valign="top" class="aa">Title ::</td>
+                <td width="300" valign="top"><textarea name="textarea" id="textarea" cols="45" rows="5"></textarea></td>
+              </tr>
+              <tr>
+                <td width="200" align="right" valign="top" class="aa">&nbsp;</td>
+                <td width="200">&nbsp;</td>
+                <td width="300"><input type="submit" name="button" id="button" value="Submit" /></td>
+              </tr>
+            </table>
+          </form>
+          <p>&nbsp;</p></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><table width="975" border="0" align="center" cellpadding="5" cellspacing="5">
+        <tr>
+          <td><a href="http://lynaround.com"><img src="image/pro1.gif" width="325" height="325" border="0" /></a></td>
+          <td><a href="https://www.zara.com/th/"><img src="image/pro2.gif" width="325" height="325" border="0" /></a></td>
+          <td><a href=" http://www.madeoffabric.com/intro.html"><img src="image/pro3.gif" width="325" height="325" border="0" /></a></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td class="ceee">Copyright ©2016 EVEANDBOY <br />
+All rights reserved</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
---
+++ eveandboy/payment1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	background-image: url(image/BG.jpg);
+	text-align: center;
+	color: #FFF;
+}
+</style>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="payment.php">
+<?
+	  print"<center> <br><font size=5>Finish Ordered"."<br>";
+	  ?>
+</form>
+<p>
+  <input type="submit" name="button" id="button" value="Back" />
+</p>
+</body>
+</html>
